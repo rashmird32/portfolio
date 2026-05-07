9e40c6dd113f302edabce56ba424e981a16316ec
rashmird32 <rashmird32@gmail.com>
2026-05-07 18:51:59 +0530

    Updated experience and project section

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ document.querySelectorAll("a").forEach(anchor => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
